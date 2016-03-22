5018cec926cc0b08cbd5a929abb2ec8319e0d059
Tim J <timothyneiljohnson@gmail.com>
2016-03-22 18:40:53 -0500

    Update index.js

--- test/index.js
+++ test/index.js
@@ -17,15 +17,15 @@ testRule({ convention: '0' }, (tr) => {
   tr.notOk('a { border: none; }', '\'border: 0\' is preferred over \'border: none\'. (value-border-zero)');
 
   tr.ok('@media print { a { border: 0; }}');
-  tr.notOk('@media print { a { border: none; }}', '\'border: 0\' is preferred over \'border: none\'. (value-border-zero)');
+  tr.notOk('@media print { a { border: none; }}', '\'border: 0\' is preferred over \'border: none\' (value-border-zero)');
 });
 
 testRule({ convention: 'none' }, (tr) => {
   basics(tr);
 
   tr.ok('a { border: none; }');
-  tr.notOk('a { border: 0; }', '\'border: none\' is preferred over \'border: 0\'. (value-border-zero)');
+  tr.notOk('a { border: 0; }', '\'border: none\' is preferred over \'border: 0\' (value-border-zero)');
 
   tr.ok('@media print { a { border: none; }}');
-  tr.notOk('@media print { a { border: 0; }}', '\'border: none\' is preferred over \'border: 0\'. (value-border-zero)');
+  tr.notOk('@media print { a { border: 0; }}', '\'border: none\' is preferred over \'border: 0\' (value-border-zero)');
 });
